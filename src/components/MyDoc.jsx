8c08270f4573f50b86cc2624c6a8fdcddd1532cc
PushkarKhirude <pushkar.khirude10@gmail.com>
2025-07-25 12:14:22 +0530

    created the skeleton for invoice template, styling needs to be done.

--- src/components/MyDoc.jsx
+++ src/components/MyDoc.jsx
@@ -4,13 +4,19 @@ import { Page, Text, View, Document, StyleSheet } from "@react-pdf/renderer";
 // Create styles
 const styles = StyleSheet.create({
   page: {
-    flexDirection: "row",
+    flexDirection: "col",
     backgroundColor: "#ffffff",
   },
-  section: {
+  row: {
+    flexDirection: "row",
+    border: "1",
+  },
+  col: {
+    flexDirection: "col",
     margin: 10,
     padding: 10,
     flexGrow: 1,
+    border: "1",
   },
 });
 const str = "asdfasdf";
@@ -18,83 +24,122 @@ const str = "asdfasdf";
 const MyDocument = () => (
   <Document singlePage>
     <Page size="A4" style={styles.page}>
-      <View style={styles.section}>
-        <Text>Section #1</Text>
+      {/**
+       *
+       * header row
+       *
+       */}
+      <View style={styles.row}>
+        <View style={styles.col}>
+          <Text>[Company Name]</Text>
+        </View>
+        <View style={styles.col}>
+          <Text>INVOICE</Text>
+        </View>
       </View>
-      <View style={styles.section}>
-        <Text>{str}</Text>
-        <Text>Section #2</Text>
-        <Text>Section #2</Text>
-        <Text>Section #2</Text>
-        <Text>Section #2</Text>
-        <Text>Section #2</Text>
-        <Text>Section #2</Text>
-        <Text>Section #2</Text>
-        <Text>Section #2</Text>
-        <Text>Section #2</Text>
-        <Text>Section #2</Text>
-        <Text>Section #2</Text>
-        <Text>Section #2</Text>
-        <Text>Section #2</Text>
-        <Text>Section #2</Text>
-        <Text>Section #2</Text>
-        <Text>Section #2</Text>
-        <Text>Section #2</Text>
-        <Text>Section #2</Text>
-        <Text>Section #2</Text>
-        <Text>Section #2</Text>
-        <Text>Section #2</Text>
-        <Text>Section #2</Text>
-        <Text>Section #2</Text>
-        <Text>Section #2</Text>
-        <Text>Section #2</Text>
-        <Text>Section #2</Text>
-        <Text>Section #2</Text>
-        <Text>Section #2</Text>
-        <Text>Section #2</Text>
-        <Text>Section #2</Text>
-        <Text>Section #2</Text>
-        <Text>Section #2</Text>
-        <Text>Section #2</Text>
-        <Text>Section #2</Text>
-        <Text>Section #2</Text>
-        <Text>Section #2</Text>
-        <Text>Section #2</Text>
-        <Text>Section #2</Text>
-        <Text>Section #2</Text>
-        <Text>Section #2</Text>
-        <Text>Section #2</Text>
-        <Text>Section #2</Text>
-        <Text>Section #2</Text>
-        <Text>Section #2</Text>
-        <Text>Section #2</Text>
-        <Text>Section #2</Text>
-        <Text>Section #2</Text>
-        <Text>Section #2</Text>
-        <Text>Section #2</Text>
-        <Text>Section #2</Text>
-        <Text>Section #2</Text>
-        <Text>Section #2</Text>
-        <Text>Section #2</Text>
-        <Text>Section #2</Text>
-        <Text>Section #2</Text>
-        <Text>Section #2</Text>
-        <Text>Section #2</Text>
-        <Text>Section #2</Text>
-        <Text>Section #2</Text>
-        <Text>Section #2</Text>
-        <Text>Section #2</Text>
-        <Text>Section #2</Text>
-        <Text>Section #2</Text>
-        <Text>Section #2</Text>
-        <Text>Section #2</Text>
-        <Text>Section #2</Text>
-        <Text>Section #2</Text>
-        <Text>Section #2</Text>
-        <Text>Section #2</Text>
-        <Text>Section #2</Text>
-        <Text>Section #2</Text>
+      {/**
+       *
+       * company and ivoice details row
+       *
+       */}
+      <View style={styles.row}>
+        <View style={styles.col}>
+          <Text>[Stree ADDRESS]</Text>
+          <Text>[city. country. post.]</Text>
+          <Text>[phone]</Text>
+        </View>
+        <View style={{ ...styles.col, border: "3" }}>
+          <View style={{ ...styles.row, backgroundColor: "#bababa" }}>
+            <View style={styles.col}>
+              <Text>[INVOICE #]</Text>
+            </View>
+            <View style={styles.col}>
+              <Text>[Date]</Text>
+            </View>
+          </View>
+          <View style={styles.row}>
+            <View style={styles.col}>
+              <Text>[INV123]</Text>
+            </View>
+            <View style={styles.col}>
+              <Text>[31/05/2001]</Text>
+            </View>
+          </View>
+        </View>
       </View>
+      {/**
+       *
+       * Bill To Row
+       *
+       */}
+      <View style={styles.row}>
+        <View style={styles.col}>
+          <Text>BILL TO</Text>
+          <Text>[Client Name]</Text>
+          <Text>[Email]</Text>
+          <Text>[Street Address]</Text>
+          <Text>[city . country . po]</Text>
+          <Text>[Phone Number]</Text>
+        </View>
+      </View>
+      {/**
+       *
+       * Item Details Row
+       *
+       */}
+      <View style={styles.row}>
+        <View style={styles.col}>
+          <Text>ITEMS</Text>
+        </View>
+        <View style={styles.col}>
+          <Text>AMOUNT</Text>
+        </View>
+      </View>
+      <View style={styles.row}>
+        <View style={styles.col}>
+          <Text>service fee</Text>
+        </View>
+        <View style={styles.col}>
+          <Text>200</Text>
+        </View>
+      </View>
+      <View style={styles.row}>
+        <View style={styles.col}>
+          <Text>service fee</Text>
+        </View>
+        <View style={styles.col}>
+          <Text>200</Text>
+        </View>
+      </View>
+      <View style={styles.row}>
+        <View style={styles.col}>
+          <Text>Thank You for you Business!</Text>
+        </View>
+        <View style={styles.row}>
+          <Text>TOTAL </Text>
+          <Text>400 </Text>
+        </View>
+      </View>
+
+      {/**
+       *
+       * Contact Details Row
+       *
+       */}
+      <View style={styles.row}>
+        <View style={styles.col}>
+          <Text>
+            If you have any questions about this invoice, please contact
+          </Text>
+          <Text>[Name, Phone, Email@address.com]</Text>
+        </View>
+      </View>
+      {/**
+       *
+       * Footer row
+       *
+       */}
+      <View></View>
     </Page>
   </Document>
 );
--- src/components/PreviewPane.jsx
+++ src/components/PreviewPane.jsx
@@ -4,9 +4,9 @@ import MyDocument from "./MyDoc";
 
 const PreviewPane = () => {
   return (
-    <div className="preview-container border flex-col  h-[90vh] ">
+    <div className="preview-container border flex-col h-[90vh]">
       <h1 className="font-bold text-lg my-3 text-gray-400">Preview</h1>
-      <PDFViewer style={{ width: "100%", height: "100%" }}>
+      <PDFViewer style={{ width: "100%", height: "92%" }}>
         <MyDocument />
       </PDFViewer>
     </div>
