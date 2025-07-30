a48df4824c979bf5c060e5e6987b8db1cb7a4d18
PushkarKhirude <pushkar.khirude10@gmail.com>
2025-07-30 22:08:33 +0530

    minor fix: provided key to pdfDownloadLink

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useRef } from "react";
 import formDataObj from "../utils/formDataObject";
 import MyDocument from "./MyDocument";
 import { PDFDownloadLink } from "@react-pdf/renderer";
@@ -6,6 +6,10 @@ import FormDataContext from "../utils/FormDataContext";
 
 const Header = ({ setFormData }) => {
   const data = useContext(FormDataContext);
+  const count = useRef(0);
+  useEffect(() => {
+    count.current++;
+  });
   return (
     <div className="header  flex justify-between border-b bg-blue-50 border-blue-800">
       <button
@@ -15,6 +19,7 @@ const Header = ({ setFormData }) => {
         Clear All ✖
       </button>
       <PDFDownloadLink
+        key={count.current}
         document={<MyDocument data={data} />}
         fileName={`${formDataObj.id}.pdf`}
         className="border hover:shadow-xl p-2 rounded-lg font-bold mx-auto my-5 cursor-pointer hover:bg-green-300 active:bg-green-500 bg-blue-200"
